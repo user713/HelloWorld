017c349069fca71520cdf7d4538fd66274b3d50c
mdunn <mjdmmv@hotmail.com>
2017-12-03 23:02:13 -0600

    Added a Web API for distributing messages as defined by the business team.

---
+++ ConsoleApp1/WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        /// <summary>
+        /// Returns the message as specified in the business specs.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
